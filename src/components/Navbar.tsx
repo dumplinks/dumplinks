4be6ba11fea10bd5871abc18884d2f7210f6112a
Geniuska275 <aigbojie2020@gmail.com>
2026-01-09 13:16:19 +0100

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const isActive = (path: string) => location.pathname === path;
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-card/95 bg-transparent border-b border-border">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-card/95  bg-white md:bg-transparent border-b border-border">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
